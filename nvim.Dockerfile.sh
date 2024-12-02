61fbf95b933409faa4b0a8675ff0f3091aad3ac6
Dennis Sandler <sandler.denis@gmail.com>
2024-12-01 21:46:12 -0500

    Work-in-progress. Need to work the coc-settings.json.

--- nvim.Dockerfile.sh
+++ nvim.Dockerfile.sh
@@ -1,9 +1,42 @@
 #!/bin/bash
 
-# docker build base_ide layer (so builder + next layer). Tag it with running name.
+SCRIPT_DIR=$(dirname $(realpath $0))
+
+print_usage()
+{
+  echo "Usage: $(basename $0) base-image language-targets..." >&2
+}
+
+BASE_IMAGE=$1
+if [ -z $BASE_IMAGE ]
+then
+  echo "ERROR: Please specify base images. Exiting." >&2
+  print_usage
+fi
+
+if [ -z $2 ]
+then
+  echo "ERROR: Need at least one language target. Exiting." >&2
+  print_usage
+fi
+
+RUNNING_TAG=$( echo "$BASE_IMAGE" | sha1sum | grep -o ^[a-f0-9]* )
+
+docker build -t $RUNNING_TAG --target nvim_ide_base -f $SCRIPT_DIR/nvim.Dockerfile --build-arg from=$BASE_IMAGE
 
 # Loop for through arg
-## add matching arg to language layer and add to coc-settings.json 
-## build language layer with FROM=running-name. 
+for LANG_TARGET in "${@:2}"
+do
+  RUNNING_TAG_NEXT=$( echo "$RUNNING_TAG $LANG_TARGET" | sha1sum | grep -o ^[a-f0-9]* )
+
+  ## add matching arg to language layer and add to coc-settings.json 
+  ## TODO
+
+  ## build language layer with FROM=running-name. 
+  docker build -t $RUNNING_TAG_NEXT --target nvim_ide_$LANG_TARGET -f $SCRIPT_DIR/nvim.Dockerfile --build-arg from=$RUNNING_TAG
+
+  RUNNING_TAG=$RUNNING_TAG_NEXT
+done
+
 
 # docker build final layer.
